57424d3470ea87150e7ef3cfe05e6fd41d2cf25b
Raj Kumar Sharma <123236174+rajsharma4@users.noreply.github.com>
2023-07-31 21:41:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+1.
+console.log(addNumbers(5, 10));
+
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+2.
+console.log(multiplyNumbers(3,4));
+
+const multiplyNumbers = function (num1, num2){
+    return num1 * num2;
+}
+
+3.
+function addNumbers(num1, num2){
+    console.log(sum);
+    var sum;
+    sum = num1 + num2;
+    return sum;
+}
+
+console.log(addNumbers(5,10));
+
+4.
+{
+    console.log(x);
+    console.log(y);
+    console.log(z);
+
+    var x = "PW";
+    let y = "Skills";
+    const z = "!"
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+5.
+{
+    console.log(x);
+
+    let x = "hello";
+
+    console.log(x);
+}
